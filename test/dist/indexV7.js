55d9eadcdee67d456d4b3b4c0a6c7a8c9645d2a0
Nanna <5608562@RTS-365.dk>
2019-05-22 10:27:27 +0200

    færdig indexv7

--- test/dist/indexV7.js
+++ test/dist/indexV7.js
@@ -3,16 +3,6 @@ document.addEventListener('DOMContentLoaded', function(){
 // opret administrator
 let institutionManager = new InstitutionAdmin("Admin");
 // console.log(institutionManager);
-let statsManager = new StatsManager(institutionManager);
-let classHidePage = "hide-page-content";
-let classShowPage = "show-page-content"
-let navManager = new NavManager(classHidePage, classShowPage);
-let showDataManager = new PageShowData(institutionManager);
-
-// REFERENCER TIL HTML
-let testButtonWriteToDOM = document.querySelector(".test-button2");
-let cssSelector = "#list-container";
-
 
 // HENTER DATA
 fetch("data/data.json")
@@ -61,17 +51,20 @@ jsonInstitutions.forEach(jsoninstitution => {
             });
         })
     });
-
 });
 
 // console.log(institutionManager);
 
+// VARIABLER TIL NAV MANAGER
+let classHidePage = "hide-page-content";
+let classShowPage = "show-page-content";
 
-// når data indsættes første gang, skal der tilføjes et dataset-index.
-// til dette array skal hentes index-tal fra data-set hver gang der trykkes på et nyt objekt
+// VARIABLER TIL PAGE EDIT DATA
+let editDataCssSelector = "#edit-data-container";
+let editDataContainer = document.querySelector(editDataCssSelector);
+
 let indexArray = [];
 
-// arrayets data skal fordeles til nedenstående variabler i rækkefølge via array metode.
 let institutionIndex;
 let educationIndex;
 let schoolClassIndex;
@@ -82,45 +75,58 @@ let selectedEducation;
 let selectedSchoolClass;
 let selectedStudent;
 
-function updateSelectedPath(indexarray){
-    institutionIndex = indexarray[0];
-    educationIndex = indexarray[1];
-    schoolClassIndex = indexarray[2];
-    studentIndex = indexarray[3];
-}
-
-// KALDER MIN PAGEEDITDATA
-let editDataCssSelector = "#edit-data-container";
-let editDataContainer = document.querySelector(editDataCssSelector);
-let editDataManager = new PageEditData(institutionManager, editDataCssSelector);
-editDataManager.writeSelectedInstitutionsToDOM();
-// tilføjer dataset.index
 let editDataLinksArray;
 
-addDatasetIndex();
+let testButtonWriteToDOM = document.querySelector(".test-button2");
+let cssSelector = "#list-container";
+
+// INITIALISE DEPENDENT CONSTRUCTORS
+let statsManager = new StatsManager(institutionManager);
+let navManager = new NavManager(classHidePage, classShowPage);
+let showDataManager = new PageShowData(institutionManager);
+let editDataManager = new PageEditData(institutionManager, editDataCssSelector);
+
+// INIT STATSMANAGER FUNCTION
+
+// INIT NAVMANAGER FUNCTIONS
+
+// INIT SHOWDATAMANAGER FUNCTIONS
+
+// INIT EDITDATAMANAGER FUNCTIONS
+// init skriv alle institutioner til dom
+editDataManager.writeSelectedInstitutionsToDOM();
+// init prepare page, som gør siden klar til at blive redigeret.
+preparePageEdit(institutionManager.institutions);
+
+// ---------------------- EDIT DATA PAGE ------------------------------ 
+
+// addDatasetIndex();
 // TILFØJER DATASET TIL ALLE LIST-ITEMS SOM EKSISTERER NU I DOM'EN
 function addDatasetIndex(){
-    editDataLinksArray = document.querySelectorAll(`${editDataCssSelector} li`);
+    editDataLinksArray = queryArray(`${editDataCssSelector} li`);
     editDataLinksArray.forEach((link, index) => {
         link.dataset.index = index;
         // console.log("har tilføjet dataset.index");
     })
 }
 
-addClickEvent();
+// addClickEvent();
 function addClickEvent(){
-    editDataLinksArray = document.querySelectorAll(`${editDataCssSelector} li`);
+    editDataLinksArray = queryArray(`${editDataCssSelector} li`);
     editDataLinksArray.forEach((link) => {
         // console.log("har tilføjet click event");
         link.addEventListener('click' , (event) => {
-            // noget mere
-            getCurrentLinkIndex(event);
-            updateSelectedPath(indexArray);
-            selectedByIndex();
+            if(event.target == event.currentTarget){
+                // noget mere
+                getCurrentLinkIndex(event);
+                updateSelectedPath(indexArray);
+                selectedByIndex();
+            }
         });
     })
 }
 
+// tilhører addClickEvent
 function getCurrentLinkIndex(event) {
     // console.log("kører getCurrentLinkIndex");
     let linkElement;
@@ -130,40 +136,51 @@ function getCurrentLinkIndex(event) {
         linkIndex = linkElement.dataset.index;
         // console.log(linkIndex);
         indexArray.push(linkIndex);
+        // console.log(indexArray);
         return indexArray;
     } 
 }
 
+// tilhører addClickEvent
+function updateSelectedPath(indexarray){
+    // console.log("kører updateSelectedPath");
+    institutionIndex = indexarray[0];
+    // console.log("institutionindex =" + institutionIndex);
+    educationIndex = indexarray[1];
+    schoolClassIndex = indexarray[2];
+    studentIndex = indexarray[3];
+}
+
+// tilhører addClickEvent
 function selectedByIndex(){
     if(indexArray.length > 0) {
         clearDataContainer();
-        // det kan godt være jeg skal lave min 2. betingelse om, hvis input bliver en string
         if(institutionIndex != undefined) {
             // console.log("its a hit");
             selectedInstitution = institutionManager.institutions[institutionIndex];
             // HER SKAL MAN SÅ SKRIVE SELECTEDINSTITUTIONS.EDUCATIONS TIL DOM'EN....
             editDataManager.writeSelectedEducationsToDOM(selectedInstitution);
             // console.log(selectedInstitution);
-            addDatasetIndex();
-            addClickEvent();
-            removeButtonEvent(selectedInstitution.educations);
+            // preparePageEdit samler alle nedenstående functions.
+            preparePageEdit(selectedInstitution.educations);
+            // addDatasetIndex();
+            // addClickEvent();
+            // removeDataButtonEvent(selectedInstitution.educations);
+            // editDataButtonEvent(selectedInstitution.educations);
+
             if(educationIndex != undefined) {
                 clearDataContainer();
                 selectedEducation = selectedInstitution.educations[educationIndex];
                 editDataManager.writeSelectedSchoolClassesToDOM(selectedEducation);
                 // console.log(selectedEducation);
-                addDatasetIndex();
-                addClickEvent();
-                removeButtonEvent(selectedEducation.schoolclasses);
+                preparePageEdit(selectedEducation.schoolclasses);
 
                 if(schoolClassIndex != undefined) {
                     clearDataContainer();
                     selectedSchoolClass = selectedEducation.schoolclasses[schoolClassIndex];
                     // console.log(selectedSchoolClass);
                     editDataManager.writeSelectedStudentsToDOM(selectedSchoolClass);
-                    addDatasetIndex();
-                    addClickEvent();
-                    removeButtonEvent(selectedSchoolClass.students);
+                    preparePageEdit(selectedSchoolClass.students);
 
                     if(studentIndex != undefined) {
                         clearDataContainer();
@@ -171,7 +188,7 @@ function selectedByIndex(){
                         // console.log(selectedStudent);
                         addDatasetIndex();
                         // addClickEvent();
-
+                        // editDataButtonEvent(selectedStudent.student);
                     } else {
                         console.log("eleven findes ikke");
                     }
@@ -189,70 +206,93 @@ function selectedByIndex(){
     }
 }
 
-// initialiserer removeButtonEventet på institutions
-removeButtonEvent(institutionManager.institutions);
 
-// removeButtonEvent, som fjerner punkter på listen
-function removeButtonEvent(array){
+// tilhører selectByIndex
+// removeDataButtonEvent, som fjerner punkter på listen
+function removeDataButtonEvent(array){
     // console.log("initial" + array.length)
     let buttonElementsArray = document.querySelectorAll("#page-3 .remove-data-button");
+    // console.log(buttonElementsArray);
     buttonElementsArray.forEach(buttonelement => {
    
         buttonelement.addEventListener('click', (event) => {
-
-            let linkIndex = getParentNodeIndex(buttonelement);
-            // console.log(linkIndex);
-            // // fjerne item med linkIndex;
-            if(array.length >= 2) {
-                array.splice(linkIndex, 1);
-                // console.log("array length " + array.length);
-                // console.log(array);
-                array.forEach(item => {
+            event.preventDefault();
+            if(event.target == event.currentTarget){                
+                let linkIndex = getParentNodeIndex(buttonelement);
+                // console.log(linkIndex);
+                // // fjerne item med linkIndex;
+                if(array.length >= 2) {
+                    array.splice(linkIndex, 1);
+                    // console.log("array length " + array.length);
+                    // console.log(array);
+                    array.forEach(item => {
+                        editDataContainer.innerHTML = "";
+                        StaticClasses.createEditedData(item, editDataCssSelector);
+                        addClickEvent();
+                        removeDataButtonEvent(array);
+                    })
+                } else if(array.length == 1){
+                    // console.log("array length " + array.lenght);
+                    array = [];
                     editDataContainer.innerHTML = "";
-                    StaticClasses.createEditedData(item, editDataCssSelector);
-                    addClickEvent();
-                    removeButtonEvent(array);
-                })
-            } else if(array.length == 1){
-                // console.log("array length " + array.lenght);
-                array = [];
-                editDataContainer.innerHTML = "";
+                }
             } 
         });
     })
 }
 
-
-
 let formTemplate = document.formtemplate;
 let clonedForm = formTemplate.cloneNode(true);
 // let selectedExistingName = clonedForm.querySelector(".form-template__existing-name").innerHTML;
 
-
-openFormEvent(institutionManager.institutions);
-function openFormEvent(array){
+// tilhører selectByIndex
+function editDataButtonEvent(array){
     // kan laves om til if(event.target == event.currentTarget) > parentNode.dataset.index
     // let buttonElement = document.querySelector("#page-3 .edit-data-button");
-    let buttonElementArray = getArray("#page-3 .edit-data-button");
+    let buttonElementArray = document.querySelectorAll("#page-3 .edit-data-button");
+    console.log(buttonElementArray);
 
     buttonElementArray.forEach(button => {
             button.addEventListener('click', (event) => {
                 event.preventDefault();            
-            if(event.target == event.currentTarget) {
-                let linkElementIndex = getParentNodeIndex(button);
-                // console.log(linkElementIndex);
-                let selectThisObjectToEdit = array[linkElementIndex];
+                if(event.target == event.currentTarget) {
+                    console.log("currentTarget = edit-data-button");
+                    let linkElementIndex = getParentNodeIndex(button);
+                    console.log(linkElementIndex);
+                    let selectThisObjectToEdit = array[linkElementIndex];
 
-                updateOpenFormUI(selectThisObjectToEdit);
+                    updateFormUI(selectThisObjectToEdit);
 
-                submitEditedData(array, selectThisObjectToEdit);
-            }
+                    updatePageUI(array, selectThisObjectToEdit);
+                }
         });
     }) 
 }
 
 
-function submitEditedData(array, selecttoedit) {
+// tilhører remove & edit button-events
+// skaffer et parent-elements dataset.index
+function getParentNodeIndex(element) {
+    let parentNode = element.parentNode;
+    let parentNodeIndex = parentNode.dataset.index;
+    return parentNodeIndex;
+}
+
+// hjælpefunction, i bund og grund en queryselectAll
+// kan evt komme i staticclasses.
+function queryArray(cssSelector) {
+    let array = document.querySelectorAll(cssSelector);
+    return array;
+}
+
+// ---------------------- EDIT DATA PAGE - UPDATE UI ------------------------------ 
+function updateFormUI(selectedobject) {
+    clonedForm.children[1].innerHTML = selectedobject.name;
+    clearDataContainer();
+    editDataContainer.appendChild(clonedForm);
+}
+
+function updatePageUI(array, selecttoedit) {
 
     let buttonSubmitEditedData = clonedForm.querySelector(".form-template__submit-form");
     // console.log(buttonSubmitEditedData);
@@ -260,34 +300,21 @@ function submitEditedData(array, selecttoedit) {
     buttonSubmitEditedData.addEventListener('click', (event) => {
         event.preventDefault();
         let formInput = clonedForm.editname.value;
-        // console.log(formInput);
-        // ER NÅET HER TIL, SKAL TIL AT OPDATERE UI
-        updateSubmitFormUI();
         selecttoedit.name = formInput;
         clearDataContainer();
         array.forEach(item => {
             StaticClasses.createEditedData(item, editDataCssSelector);
         });
+        preparePageEdit(array);
     });
 }
 
-// skaffer linkElementets dataset.index
-function getParentNodeIndex(element) {
-    let parentNode = element.parentNode;
-    let parentNodeIndex = parentNode.dataset.index;
-    return parentNodeIndex;
-}
-
-
-function getArray(cssSelector) {
-    let array = document.querySelectorAll(cssSelector);
-    return array;
-}
-
-function updateOpenFormUI(selectedobject) {
-    clonedForm.children[1].innerHTML = selectedobject.name;
-    clearDataContainer();
-    editDataContainer.appendChild(clonedForm);
+function preparePageEdit(array) {
+    // NB KØRER DISSE 4 FUNCTIONER SAMLET, FLERE GANGE, SKAL LÆGGES IND I FUNCTION
+    addDatasetIndex();
+    addClickEvent();
+    removeDataButtonEvent(array);
+    editDataButtonEvent(array);
 }
 
 // function som tømmer min data-container, så der kan indsættes en ny liste
